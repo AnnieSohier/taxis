b00f25ebf6c64f33e6eb2d67f08269a70d8be979
SOHIER ANNIE <annie.sohier@infodb.fr>
2014-04-23 14:34:50 +0200

    test de commit

---
+++ taxis/src/main/java/test/taxis/App.java
@@ -0,0 +1,13 @@
+package test.taxis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Appli gérée sur GitHub" );
+    }
+}
